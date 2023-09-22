e4783d36cd2913696f15f9182d725877eed101c3
Jessie <hire.jesslw@gmail.com>
2023-09-21 17:57:38 -1000

    Add json2csv library for output.csv

--- scrape.js
+++ scrape.js
@@ -1,42 +1,61 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const { Parser } = require('json2csv');
 
 (async () => {
   const browser = await puppeteer.launch();
 
   try {
-    const domains = ['https://www.example.com', 'https://www.test.com'];
-    const paths = ['/page1', '/page2', '/page3']; // Add more paths as needed
+    const domains = [
+      'https://www.example.com/', 
+      'https://www.test.com/'
+    ];
+    const paths = [
+      'path1',
+      'path2',
+      'path3'
+      ]; // Add more paths as needed
 
     const allAnchorData = [];
+    //array for scraped data to be pushed into
 
     for (const domain of domains) {
       for (const path of paths) {
         const url = domain + path;
         const page = await browser.newPage();
-        await page.goto(url);
+        await page.goto(url, {'timeout': 120000});
+        //added timeout for pages with long loading times, lots of videos, etc.
 
-        const anchorData = await page.evaluate(url => {
+        const anchorData = await page.evaluate((domain, path) => {
+          //create an array of all anchor tags with class='btn-primary'
           const anchorTags = Array.from(document.querySelectorAll('a.btn-primary'));
+          //return the array as JSON with selected content
           return anchorTags.map(tag => {
             return {
-              url: url,
+              //return both domain & path to compare differences
+              domain: domain,
+              path: path,
               href: tag.getAttribute('href'),
               text: tag.textContent.trim()
             };
           });
-        }, url);
+        }, domain, path);
 
         allAnchorData.push(...anchorData);
         await page.close();
       }
     }
 
-    // Specify the output file path
-    const outputFile = 'output.json';
+  // Specify the output file path
+  const outputFile = 'output.csv';
 
-    // Write the extracted data to the output file as JSON
-    fs.writeFileSync(outputFile, JSON.stringify(allAnchorData, null, 2));
+  // Convert JSON data to CSV format
+  const json2csvParser = new Parser();
+  const csv = json2csvParser.parse(allAnchorData);
+
+  // Write the CSV data to the output file
+  fs.writeFileSync(outputFile, csv);
+
 
     console.log(`Anchor data written to ${outputFile}`);
   } catch (error) {
